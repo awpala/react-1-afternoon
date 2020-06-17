416369f42d97fbc7c6e8aa4ec83e5e3d53738b20
awpala <adam.w.pala@gmail.com>
2020-06-17 17:14:19 -0500

    completed Step 4 FilterObject.js

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -33,7 +33,7 @@ export default class EvenAndOdd extends Component {
       // <p> EvenAndOdd Component </p>
       <div className="puzzleBox evenAndOddPB">
         <h4>Evens and Odds</h4>
-        <input className="inputLine" onChange= { e => this.handleChange(e.target.value) } />
+        <input className="inputLine" onChange={ e => this.handleChange(e.target.value) } />
         <button className="confirmationButton" onClick={ () => { this.assignEvenAndOdds(this.state.userInput) }}> Split </button>
         <span className="resultsBox"> Evens: { JSON.stringify(this.state.evenArray) }</span>
         <span className="resultsBox"> Odds: { JSON.stringify(this.state.oddArray) }</span>
